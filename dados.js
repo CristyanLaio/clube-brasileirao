8379a2a82d554e04c5bee37aeaeb24d105dcd8c6
laio <cristyanlaio@protonmail.com>
2024-09-06 19:14:27 -0300

    Arquivos do site sendo adicionados

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Flamengo",
+        descricao: "O Clube de Regatas do Flamengo (mais conhecido simplesmente como Flamengo, popularmente pelos apelidos de Fla, Mengo e Mengão, e cujo acrônimo é CRF) é uma agremiação poliesportiva brasileira com sede na cidade do Rio de Janeiro, capital do estado homônimo.",
+        link: "https://www.flamengo.com.br/"
+    },
+        {titulo: "Vasco",
+        descricao: "O Club de Regatas Vasco da Gama ComC • MHM • MHIH • EmMJK • MNVG • MMDM • MAPAM • MT • MSFA, mais conhecido como Vasco da Gama ou simplesmente Vasco, cujo acrônimo é CRVG, é uma entidade sócio-poliesportiva brasileira com sede na cidade do Rio de Janeiro, fundada em 21 de agosto de 1898 por um grupo de remadores.",
+        link: "https://vasco.com.br/"
+    },
+    {
+        titulo: "Corinthians",
+        descricao: "O Sport Club Corinthians Paulista, comumente referido como Corinthians, ou ainda pelo seu acrônimo SCCP, é um clube poliesportivo brasileiro da cidade de São Paulo, capital do estado de São Paulo. Foi fundado como uma equipe de futebol no dia 1 de setembro de 1910 por um grupo de operários anarco-sindicalistas do bairro Bom Retiro. Seu nome foi inspirado no Corinthian Football Club de Londres, que excursionava pelo Brasil.",
+        link: "https://www.corinthians.com.br/"
+    }
+]
+
